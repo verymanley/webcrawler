8f0e8d7910f04402c7c867dbc21b35959c5d9cb5
verymanley <verymanley@protonmail.com>
2018-05-22 19:03:03 -0400

    update crawler.py

--- crawler.py
+++ crawler.py
@@ -5,5 +5,7 @@
 # speed_of_light in meters per second
 # cycles_per_second is 2.7 GHz
 
-speed_of_light = 299792458.0
-cycles_per_second = 2700000000.0
+SOL = 299792458.0
+CPS = 2700000000.0
+
+print SOL * CPS
