ba302e0a3a4a8bb8a6ebbca9f7646cb1d853eb07
verymanley <verymanley@protonmail.com>
2018-05-28 17:43:48 -0400

    update crawler.py

--- crawler.py
+++ crawler.py
@@ -10,3 +10,10 @@ end_quote = page.find('"',start_quote+1)
 url = page[start_quote+1:end_quote]
 
 print url
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    start_quote = page.find('"',start_link)
+    end_quote = page.find('"',start_quote+1)
+    url = page[start_quote+1:end_quote]
+    return url,end_quote
