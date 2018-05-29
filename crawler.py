06c092025d3638828df36f1875678caeac9e8ec9
verymanley <verymanley@protonmail.com>
2018-05-29 19:34:05 -0400

    update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,19 +1,22 @@
 #webcrawler v0.01
 
 # page = contents of a web page
-page =('<div id="top_bin"><div id="top_content" class="width960">'
-'<div class="udacity float-left"><a href="http://udacity.com">')
-
+page = ('<div class="udacity float-left"><a href="http://udacity.com">')
 start_link = page.find('<a href=')
 start_quote = page.find('"',start_link)
 end_quote = page.find('"',start_quote+1)
 url = page[start_quote+1:end_quote]
-
 print url
 
 def get_next_target(page):
     start_link = page.find('<a href=')
-    start_quote = page.find('"',start_link)
-    end_quote = page.find('"',start_quote+1)
-    url = page[start_quote+1:end_quote]
-    return url,end_quote
+
+    # if the link tag sequence is not found, find returns a -1
+    if start_link == -1:
+        # return the error codes of None, 0 now and skip the rest!
+        return None, 0
+
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
