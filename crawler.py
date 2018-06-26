455d29e73bed845b0db82895ed87469ab7b1a1ab
verymanley <verymanley@protonmail.com>
2018-06-26 17:04:50 -0400

    update crawler.py

--- crawler.py
+++ crawler.py
@@ -6,7 +6,7 @@ start_link = page.find('<a href=')
 start_quote = page.find('"',start_link)
 end_quote = page.find('"',start_quote+1)
 url = page[start_quote+1:end_quote]
-print url
+print (url)
 
 def get_next_target(page):
     start_link = page.find('<a href=')
